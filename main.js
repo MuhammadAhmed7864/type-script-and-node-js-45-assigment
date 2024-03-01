08d2237a7566ad00a267a452e948a346327096a1
Muhammac Ahmed <161842334+MuhammadAhmed7864@users.noreply.github.com>
2024-03-01 20:33:44 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
-var personName = "Eric";
-console.log("Hello ".concat(personName, ",would you like to learn some phyton today?"));
+var quote = "a person who never made a mistake never tryied any thing new";
+var author = "Albert Einstein";
+console.log("".concat(author, " once said,\"").concat(quote, "\""));
+var famous_person = "Albert Einstein";
+var message = ("".concat(famous_person, " once said,\"").concat(quote, "\""));
+console.log(message);
--- main.ts
+++ main.ts
@@ -1,2 +1,8 @@
-let personName: string="Eric";
-console.log(`Hello ${personName},would you like to learn some phyton today?`);
+let quote: string = "a person who never made a mistake never tryied any thing new";
+let author: string ="Albert Einstein"
+
+console.log(`${author} once said,"${quote}"`);
+
+let famous_person: string = "Albert Einstein";
+let message: string = (`${famous_person} once said,"${quote}"`);
+console.log(message);

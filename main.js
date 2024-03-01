ee80e14173f8d4d439bd35925edf16342ea83144
Muhammac Ahmed <161842334+MuhammadAhmed7864@users.noreply.github.com>
2024-03-01 21:47:45 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
-var days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
-// console.log(days{7}) 
-console.log(days[5]);
+// Task 23 
+var car = "subaru";
+console.log("is car == 'subaru'? predict true");
+console.log(car == 'subaru');
+console.log("is car !='DHOOM'? predic true");
+console.log(car != 'DHOOM');
+console.log("is car == 'subaru'? predict false");
+console.log(car == 'subaru');
+console.log("is car == 'SUBARU'? predict false");
+console.log(car == 'SUBARU');
+console.log("is car length ==6? predict true");
+console.log(car.length == 6);
+console.log("is car length !=10? predict true");
+console.log(car.length != 10);
+console.log("is 10 > 50 ? predict false");
+console.log(10 > 45);
+console.log("is 3 <= 2 ? predict false");
+console.log(3 <= 2);
+console.log("is 3 <= 2 ? predict false");
+console.log(3 <= 2);
+console.log("is 75 <= 60 ? predict false");
+console.log(75 <= 60);
+console.log("is car == 'subaru' && car.length == 6? predict true");
+console.log(car == 'subaru' && car.length == 6);

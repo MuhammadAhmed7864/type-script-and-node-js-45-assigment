700032f3a4539c44b11062df5397405712ea42fe
Muhammac Ahmed <161842334+MuhammadAhmed7864@users.noreply.github.com>
2024-03-01 21:43:02 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,4 @@
-// part 1 Orignal order
-var placeToVisit = ["Pakistan", "china", "SaudiArabia", "Phalestine"];
-console.log("Orignal order:", placeToVisit);
-// part 2 alphabetic order 
-console.log("Alphabetical order:", placeToVisit.sort());
-// part 3 Orignal order after sorting
-console.log("Orignal order after sorting:", placeToVisit);
-// part 4 Reverse alphabetical order 
-console.log("Reverse alphabetical order:", placeToVisit.sort().reverse());
-// part 5 orignal order after reverse sorting 
-console.log("Orignal after order sorting:", placeToVisit);
-// part 6 reaverse order 
-placeToVisit.reverse();
-console.log("Reversed order:", placeToVisit);
-// part 7 sort the array in alphabetical order 
-placeToVisit.sort();
-console.log("Sorted in alphabetical order:", placeToVisit);
-// part 8 reverse alphabetical order 
-placeToVisit.sort(function (a, b) { return b.localeCompare(a); });
-console.log("Sorted in reverse alphabetical order", placeToVisit);
+// Task 19 
+var invitation = ["AHMED", "ASAAD",];
+var count_invitation = invitation.length;
+console.log("".concat(count_invitation, " people will come to the dinner"));
--- main.ts
+++ main.ts
@@ -1,27 +1,5 @@
-// part 1 Orignal order
-let placeToVisit: string[] = ["Pakistan","china","SaudiArabia","Phalestine"];
-console.log("Orignal order:", placeToVisit )
+// Task 19 
+let invitation : string [] = ["AHMED","ASAAD",]
+let count_invitation : number = invitation.length
 
-// part 2 alphabetic order 
-console.log("Alphabetical order:",placeToVisit.sort());
-
-// part 3 Orignal order after sorting
-console.log("Orignal order after sorting:", placeToVisit);
-
-// part 4 Reverse alphabetical order 
-console.log("Reverse alphabetical order:",placeToVisit.sort().reverse());
-
-// part 5 orignal order after reverse sorting 
-console.log("Orignal after order sorting:",placeToVisit);
-
-// part 6 reaverse order 
-placeToVisit.reverse();
-console.log("Reversed order:", placeToVisit);
-
-// part 7 sort the array in alphabetical order 
-placeToVisit.sort();
-console.log("Sorted in alphabetical order:", placeToVisit);
-
-// part 8 reverse alphabetical order 
-placeToVisit.sort((a,b) => b.localeCompare(a));
-console.log("Sorted in reverse alphabetical order",placeToVisit); 
+console.log(`${count_invitation} people will come to the dinner`) 

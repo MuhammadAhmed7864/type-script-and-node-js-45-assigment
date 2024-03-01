cce676a19943a1892bba2f3367046fe9ffc6dd1a
Muhammac Ahmed <161842334+MuhammadAhmed7864@users.noreply.github.com>
2024-03-01 20:36:19 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,2 @@
-console.log(5 + 3);
-console.log(12 - 4);
-console.log(4 * 2);
-console.log(16 / 2);
+var favoriteNumber = 5;
+console.log("My favorite Number is ".concat(favoriteNumber, ","));
--- main.ts
+++ main.ts
@@ -1,4 +1,9 @@
-console.log(5+3);
-console.log(12-4);
-console.log(4*2);
-console.log(16/2);
+// author: [M AHMED SOHAIL]
+// date: [wednesday,Febuary ]
+
+// task 9: printing my favorite number
+
+let favoriteNumber: number=5;
+
+// revelving my favorite number in a message formate
+console.log(`My favorite Number is ${favoriteNumber},`);

d5ff2f6355d863409132b594b8d119402d32891d
Muhammac Ahmed <161842334+MuhammadAhmed7864@users.noreply.github.com>
2024-03-01 22:06:02 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,12 @@
-var favourite_fruits = ["Mango", "orange", "apple", "peach", "berry"];
-if (favourite_fruits.includes("Mango")) {
-    console.log("Mango");
-}
-if (favourite_fruits.includes("peach")) {
-    console.log("you really like Bananas");
-}
-if (favourite_fruits.includes("orange")) {
-    console.log("you really like Bananas");
-}
-if (favourite_fruits.includes("berry")) {
-    console.log("you really like Bananas");
-}
-if (favourite_fruits.includes("apple")) {
-    console.log("you really like Bananas");
+var current_users = ["admin", "Ahmed", "Asaad", "Hiba", "sohail"];
+var new_users = ["admin", "sohail", "Hussain", "Faisal", "Iqbal"];
+var current_users_lower = current_users.map(function (user) { return user.toLowerCase(); });
+for (var _i = 0, new_users_1 = new_users; _i < new_users_1.length; _i++) {
+    var new_user = new_users_1[_i];
+    if (current_users_lower.includes(new_user.toLowerCase())) {
+        console.log("Soory ".concat(new_user, ", that name is taken"));
+    }
+    else {
+        console.log("Yes ".concat(new_user, ", is still avaliable list"));
+    }
 }
--- main.ts
+++ main.ts
@@ -1,21 +1,14 @@
-let favourite_fruits : string [] = ["Mango","orange","apple","peach","berry"]  
+let current_users : string [] = ["admin","Ahmed","Asaad","Hiba","sohail"]
+let new_users : string [] = ["admin","sohail","Hussain","Faisal","Iqbal"]
 
-if (favourite_fruits.includes("Mango")) {
-    console.log("Mango")
-}
+let current_users_lower : string [] = current_users.map(user => user.toLowerCase())
 
-if (favourite_fruits.includes("peach")) {
-    console.log("you really like Bananas")
-}
 
-if (favourite_fruits.includes("orange")) {
-    console.log("you really like Bananas")
-}
-
-if (favourite_fruits.includes("berry")) {
-    console.log("you really like Bananas")
-}
-
-if (favourite_fruits.includes("apple")) {
-    console.log("you really like Bananas")
+for (let new_user of new_users) {
+    if (current_users_lower.includes (new_user.toLowerCase())) {
+        console.log(`Soory ${new_user}, that name is taken`)
+    } else {
+        console.log(`Yes ${new_user}, is still avaliable list`)
+    }
 } 
+ 

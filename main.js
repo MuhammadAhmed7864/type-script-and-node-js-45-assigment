38b1a6c72057641dc187f8a81eef7314fc1d36cb
Muhammac Ahmed <161842334+MuhammadAhmed7864@users.noreply.github.com>
2024-03-01 20:40:55 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
-var guestArr = ["AHMED", "ASAAD", "SOHAIL"];
-var canNotAttend = "AHMED";
-// console.log(canNotAttend + " " "can not attend dinner");
-var newGuest = "hiba";
+var guestArr = ["Hiba", "ASAAD", "SOHAIL"];
+var canNotAttend = "ASAAD";
+var newGuest = "Haram";
 guestArr[guestArr.indexOf(canNotAttend)] = newGuest;
+// console.log(guestArr)
+// guestArr.map((items) =>
+// console.log(`dear ${items} I FOUND a big dinner party tonight I AM INVITED MORE PEOPLE.`)
+// );
+// part 2 guest Began
+var Guestbeg = "AHMED";
+guestArr.unshift(Guestbeg);
+// console.log(guestArr)
+// part 3 guest Middle
+var middelGuest = "HUSSAIN";
+var middleindex = guestArr.length / 3;
+guestArr.splice(middleindex, 0, middelGuest);
 // console.log(guestArr);
-guestArr.map(function (items) { return (console.log("dear ".concat(items, ", you are invited to the dknner."))); });
+// part 4 append
+guestArr.push("farheen");
+console.log(guestArr);
+// part 5
+guestArr.map((function (items) {
+    return console.log("dear ".concat(items, " you are invited it the more people list of dinner"));
+}));
--- main.ts
+++ main.ts
@@ -1,11 +1,31 @@
-let guestArr : string [] = ["AHMED","ASAAD","SOHAIL"];
-let canNotAttend : string = "AHMED"
-// console.log(canNotAttend + " " "can not attend dinner");
-let newGuest : string = "hiba"
+let guestArr : string [] = ["Hiba","ASAAD","SOHAIL"];
+
+let canNotAttend : string = "ASAAD"
+
+let newGuest : string = "Haram"
+
 guestArr [guestArr.indexOf(canNotAttend)] = newGuest;
+// console.log(guestArr)
+
+// guestArr.map((items) =>
+// console.log(`dear ${items} I FOUND a big dinner party tonight I AM INVITED MORE PEOPLE.`)
+// );
+
+// part 2 guest Began
+let Guestbeg : string = "AHMED"
+guestArr.unshift(Guestbeg);
+// console.log(guestArr)
+
+// part 3 guest Middle
+let middelGuest : string = "HUSSAIN"
+let middleindex = guestArr.length/3
+guestArr.splice(middleindex,0,middelGuest);
 // console.log(guestArr);
-guestArr.map((items)=>(console.log(`dear ${items}, you are invited to the dknner.`)) )
 
+// part 4 append
+guestArr.push("farheen")
+console.log(guestArr)
 
-
-
+// part 5
+guestArr.map((items =>
+console.log(`dear ${items} you are invited it the more people list of dinner`));

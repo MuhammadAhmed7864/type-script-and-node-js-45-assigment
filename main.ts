f29e762e145d56e52b4e7d07c8aab661a2c57838
Muhammac Ahmed <161842334+MuhammadAhmed7864@users.noreply.github.com>
2024-03-01 22:00:40 +0500

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,15 +1,15 @@
-//   let alien_color = green"
+let age : number = 18
 
-//  if (alien_color == "green") {
-//  console.log("player just earned 5 points for shooting the alien") 
-//  } else{
-//     console.log("player just earned 10 point")
-//  } 
-
- let alien_color = "red"
-
- if (alien_color == "green") {
- console.log("player just earned 5 points for shooting the alien") 
- } else{
-    console.log("player just earned 10 point")
- } 
+if (age < 2) {
+    console.log("you are a baby")
+} else if (age < 4) {
+    console.log("you are a toddler")
+} else if (age < 13) {
+    console.log("you are a kid")
+} else if (age < 20) {
+    console.log("you are a teenager")
+} else if (age < 65) {
+    console.log("you are a adult")
+} else {
+    console.log("you are elder")
+} 
